c11ea67575d6c7cbd49171e690a83516462e2a1a
root <patcasso21@gmail.com>
2023-12-18 11:09:28 +0000

    feat: mseloss code added

--- model.py
+++ model.py
@@ -15,8 +15,8 @@ class Model(LightningModule):
         # 사용할 모델을 호출합니다.
         self.plm = transformers.AutoModelForSequenceClassification.from_pretrained(
             pretrained_model_name_or_path=model_name, num_labels=1)
-        # Loss 계산을 위해 사용될 L1Loss를 호출합니다.
-        self.loss_func = torch.nn.L1Loss()
+        # Loss 계산을 위해 사용될 MSELoss를 호출합니다.
+        self.loss_func = torch.nn.MSELoss()
 
     def forward(self, x):
         x = self.plm(x)['logits']
